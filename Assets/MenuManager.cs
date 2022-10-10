b8392721b3304616ff13582386681c382746ea8d
EnderGamez01 <adammendoza27@gmail.com>
2022-10-09 19:21:28 -0700

    MainMenu
    MainMenu

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+  public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
