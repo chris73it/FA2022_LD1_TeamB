94f1060e8135d01c411471d254cc3e2a87acc06b
EnderGamez01 <104052753+EnderGamez01@users.noreply.github.com>
2022-10-12 12:08:07 -0700

    AIChase & Tutorial
    AIChase & Tutorial

--- Assets/Scripts/AIChase.cs
+++ Assets/Scripts/AIChase.cs
@@ -6,16 +6,24 @@ public class AIChase : MonoBehaviour
 {
     public GameObject player;
     public float speed;
+    public float distanceBetween;
 
-    private float distance
+    private float distance;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; ;
+
+        if (distance < distanceBetween)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
     }
 }
