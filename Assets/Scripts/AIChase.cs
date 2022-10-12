30006a0df8aad613f5dc58ab0a11a6d16adee09e
EnderGamez01 <104052753+EnderGamez01@users.noreply.github.com>
2022-10-12 11:18:30 -0700

    Small update
    Small update

---
+++ Assets/Scripts/AIChase.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIChase : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+    private float distance
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
