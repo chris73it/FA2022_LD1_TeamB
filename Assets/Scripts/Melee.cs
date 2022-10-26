dc842376350d3869d9a200e02c78bba97aa1e8f0
WildByte <52081661+TheWildByte@users.noreply.github.com>
2022-10-26 12:10:49 -0700

    Revert "Revert "key&door""
    This reverts commit ea38532802cd9db0a771facaf218d6bc9241870d.

--- Assets/Scripts/Melee.cs
+++ Assets/Scripts/Melee.cs
@@ -14,9 +14,6 @@ public class Melee : MonoBehaviour
 
     public string exclude;
 
-    public Collider2D col;
-    public Renderer sprite;
-
     public void Setup(int dmg, string excludeName, LayerMask layer)
     {
         damage = dmg;
@@ -27,35 +24,36 @@ public class Melee : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (lifeTime < maxLife + 1)
-        {
-            lifeTime += Time.deltaTime;
-        }
+        lifeTime += Time.deltaTime;
 
+<<<<<<< HEAD
 
+=======
+        /*
+>>>>>>> parent of eba7cb4 (Melee & Projectile Fix + Update)
         if (lifeTime > maxLife)
         {
-            disable();
-        }
-    }
-
-    public void enable()
-    {
-        lifeTime = 0;
-        col.enabled = true;
-        sprite.enabled = true;
-    }
-    public void disable()
-    {
-        col.enabled = false;
-        sprite.enabled = false;
+            this.gameObject.SetActive(false);
+            if (lifeTime > maxLife + rate)
+            {
+                lifeTime = 0;
+                this.gameObject.SetActive(true);
+            }
+        }*/
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name != exclude)
         {
+<<<<<<< HEAD
             if (other.GetComponent<Character>().type != Character.CharacterType.Object)
+=======
+            Debug.Log("Melee attack connected");
+            Rigidbody rb2d = other.GetComponent<Rigidbody>();
+
+            if (rb2d != null)
+>>>>>>> parent of eba7cb4 (Melee & Projectile Fix + Update)
             {
                 Rigidbody2D rb2d = other.GetComponent<Rigidbody2D>();
 
