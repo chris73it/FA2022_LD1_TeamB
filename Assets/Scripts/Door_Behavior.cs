d310a4e60808e33a381d122ed5095a383432035f
EnderGamez01 <104052753+EnderGamez01@users.noreply.github.com>
2022-10-31 10:52:44 -0700

    boop
    some cool stuff.

--- Assets/Scripts/Door_Behavior.cs
+++ Assets/Scripts/Door_Behavior.cs
@@ -1,21 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Door_Behavior : MonoBehaviour
 {
-
+    [SerializeField]
+    private Canvas OpenDoor;
     public Key_Behavior PickedUp;
+    public int NextLevel;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (PickedUp == true && Input.GetKeyDown(KeyCode.F))
+        if (PickedUp == true)
         {
+            Debug.Log("true");
+            if (collision.gameObject.tag.Equals("Player"))
+            {
+                OpenDoor.gameObject.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    NextLevel = '3';
+                }
+            }
 
         }
         else if (PickedUp == false)
         {
-
+            if (collision.gameObject.tag.Equals("Player"))
+            {
+                OpenDoor.gameObject.SetActive(false);
+                
+            }
         }
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (PickedUp == true)
+        {
+            if (collision.gameObject.tag.Equals("Player"))
+            {
+                OpenDoor.gameObject.SetActive(false);
+                
+            }
+        }
+    }
+    private
 }
